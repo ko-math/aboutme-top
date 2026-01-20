cf494353a619f90dd4296e118b302d20e31fb0d4
ko-math <sorehakiemasu@gmail.com>
2026-01-20 10:50:44 +0900

    Add files via upload

---
+++ doThing.js
@@ -0,0 +1,59 @@
+async function doThingApi(n) {
+    const res = await fetch(`https://scratch.mit.edu/messages/ajax/user-activity/?user=ko-math&max=${n}`);
+    return await res.text();
+}
+
+async function doThing() {
+    const removeElements = document.getElementsByClassName('doThingElement');
+    for (let removeElement of removeElements){
+        removeElement.remove();
+    }
+    
+    const count =document.getElementById('doThingCount').value;
+    const doThing = await doThingApi(count);
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('doThingElement');
+    wrapper.innerHTML = doThing;
+    document.getElementById('doThingApi').appendChild(wrapper);
+}
+
+const inputDoThing =document.getElementById('doThingButton');
+inputDoThing.addEventListener("click",doThing);
+
+
+function sendHeight() {
+  const h = document.documentElement.scrollHeight;
+  window.parent.postMessage(h, '*');
+}
+
+// 初回
+window.addEventListener('load', sendHeight);
+
+// DOMが変わったら毎回
+new MutationObserver(sendHeight)
+  .observe(document.body, {
+    childList: true,
+    subtree: true
+});
+
+
+async function aboutMeApi() {
+    const res = await fetch("https://api.scratch.mit.edu/users/ko-math/");
+    return await res.json();
+}
+
+async function aboutMe() {
+    const aboutMeValue = await aboutMeApi();
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('aboutMe');
+
+    wrapper.innerHTML = `
+        <h3>${aboutMeValue.username}</h3>
+        <p>${aboutMeValue.profile?.bio ?? ""}</p>
+    `;
+
+    document.getElementById('aboutMeApi').appendChild(wrapper);
+}
+
+aboutMe();
