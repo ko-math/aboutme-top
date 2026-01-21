0f82de7977bd568befe36d58cb7909a7f8c030e6
ko-math <sorehakiemasu@gmail.com>
2026-01-21 14:55:40 +0900

    Remove toggleSub function from me.js

--- me.js
+++ me.js
@@ -13,8 +13,6 @@ const pages = {
 function loadPage(name) {
   document.getElementById('content').innerHTML = pages[name];
 }
-function toggleSub(id) {
-  document.getElementById(id).classList.toggle('hidden');
-}
+
 // 初期表示
 loadPage('about');
